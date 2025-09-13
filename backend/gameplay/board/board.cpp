628b6e69b6a123ea623fe6cb4f591d3f1cef14f2
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-13 04:58:28 +0000

    Update board functions to now display a 10x10 vector

--- backend/gameplay/board/board.cpp
+++ backend/gameplay/board/board.cpp
@@ -4,10 +4,26 @@
 using namespace std;
 
 Board::Board() {
-    
+    playerName = "Unnamed Player";
+    Rows = 10;
+    Columns = 10;
+    board = vector<vector<string>>(Rows, vector<string>(Columns, "[ ]")); // "[ ]" represents empty seea
 }
 
-Board::displayBoard() {
-
+void Board::setBoardProperties(string name, int rows, int cols) {
+    playerName = name;
+    Rows = rows;
+    Columns = cols;
+    board = vector<vector<string>>(Rows, vector<string>(Columns, "[ ]")); // Initialize board with empty sea
 }
 
+void Board::displayBoard() {
+
+    //display 2D vector
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Columns; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
