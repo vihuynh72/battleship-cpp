07ff79e2b88324efa6a4c91acdf0d495267f4d52
Vi <vihuynh27.work@gmail.com>
2025-09-14 13:05:05 -0700

    Add initial implementation of ingame functions for player turns and attacks

---
+++ backend/ui/ingame/src/ingame.cpp
@@ -0,0 +1,21 @@
+#include "../../../gameplay/board/board.h"
+#include "../../../gameplay/ship/ship.h"
+#include "../include/ingame.h"
+#include "../include/ingameHelper.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ingame(Board& player1, Board& player2) {
+
+}
+
+void playerTurn(Board& player) {
+
+}
+
+void attackTurn(Board& player, int row, int col) {
+
+}
