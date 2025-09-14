0cca8e7dff4679b00b1b92f938ef593b66d217d8
Vi <vihuynh27.work@gmail.com>
2025-09-14 12:26:18 -0700

    Add startGame function declaration and implementation

---
+++ backend/ui/ingame/src/startgame.cpp
@@ -0,0 +1,10 @@
+#include "../include/startgame.h"
+#include "../../../gameplay/board/board.h"
+#include "../../../gameplay/ship/ship.h"
+
+#include <iostream>
+#include <string>
+
+void startGame(Board& player1, Board& player2) {
+    
+}
