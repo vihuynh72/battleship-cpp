67719be51f26625214f1ef1581c862e6c57fad0e
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-14 06:04:46 +0000

    Refactor Board class: add methods to manage total ships and update board properties

--- backend/gameplay/board/board.cpp
+++ backend/gameplay/board/board.cpp
@@ -3,18 +3,29 @@
 
 using namespace std;
 
+// Constructor to initialize the board
 Board::Board() {
     playerName = "Unnamed Player";
     Rows = 10;
     Columns = 10;
-    board = vector<vector<string>>(Rows, vector<string>(Columns, "[ ]")); // "[ ]" represents empty seea
+    board = vector<vector<string>>(10, vector<string>(10, "[ ]")); // "[ ]" represents empty sea
 }
 
+// Method to set board properties
 void Board::setBoardProperties(string name, int rows, int cols) {
     playerName = name;
     Rows = rows;
     Columns = cols;
     board = vector<vector<string>>(Rows, vector<string>(Columns, "[ ]")); // Initialize board with empty sea
+}
+
+// Method to increment total ship count
+void Board::incrementTotalShips() {
+    totalShips++;
+}
+
+int Board::getTotalShips() {
+    return totalShips;
 }
 
 // TODO: Move to UI
--- backend/gameplay/board/board.h
+++ backend/gameplay/board/board.h
@@ -1,6 +1,8 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "../ship/ship.h"
+
 #include <string>
 #include <vector>
 
@@ -9,7 +11,13 @@ class Board {
         Board(); // Constructor to initialize the board
         void setBoardProperties(std::string, int, int); // Method to set board properties
         void displayBoard(); // Method to display the board
-        
+         
+        void incrementTotalShips(); // Method to increment total ship count
+        int getTotalShips(); // Method to get total ship count
+
+        void addShip(Ship&); // Method to add ship to ship list
+        std::vector<Ship> getShipList(); // Method to get list of ships
+
     private:
         // Player name
         std::string playerName;
@@ -17,6 +25,9 @@ class Board {
         // Board dimensions
         int Rows;
         int Columns;
+
+        // Ship Count
+        std::vector<Ship> shipList; // Vector to hold ships on the board
 
         // 2D vectors to represent the board for dynamic sizing
         std::vector<std::vector<std::string>> board;
