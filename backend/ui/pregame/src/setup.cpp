d0030829759944271ef6a336b2d878690795e3de
Vi <vihuynh27.work@gmail.com>
2025-09-14 13:53:47 -0700

    Refactor shipSetup function to improve variable declaration and add coordinate input for ship placement

--- backend/ui/pregame/src/setup.cpp
+++ backend/ui/pregame/src/setup.cpp
@@ -1,6 +1,8 @@
 #include "../include/setup.h"
 #include "../../gameplay/board/board.h"
 #include "../../gameplay/ship/ship.h"
+#include "../../gameplay/mechanics/ship/ship.h"
+#include "../../gameplay/mechanics/space/space.h"
 
 #include <iostream>
 
@@ -26,6 +28,7 @@ Ship shipSetup(Board& board) {
     string name;
     string direction;
     int length;
+    int ship_x_coord, ship_y_coord;
     
     cout << "Enter ship name: ";
     cin >> name;
@@ -37,7 +40,22 @@ Ship shipSetup(Board& board) {
         cout << "Invalid length. Please enter a value between 2 and 5." << endl;
         return shipSetup(board);
     }
+
+    // Create and add ship to board
     Ship ship;
     ship.setShipProperties(name, direction, length);
+
+    cout << "Enter ship starting X coordinate (0 to " << board.getColumns() - 1 << "): ";
+    cin >> ship_x_coord;
+
+    cout << "Enter ship starting Y coordinate (0 to " << board.getRows() - 1 << "): ";
+    cin >> ship_y_coord;
+
+    if (checkAvailableSpace(board, ship, ship_y_coord, ship_x_coord, direction) == false) {
+        cout << "Invalid coordinates. Please try again." << endl;
+        return shipSetup(board);
+    }
+
+    addShipToBoard(board, ship, ship_y_coord, ship_x_coord, direction);
     return ship;
 }
