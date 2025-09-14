e2dafa01708d8561d023047792e6622407b2ad68
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-14 08:28:49 +0000

    Initialize pregame UI functions and header file for game setup

---
+++ backend/ui/pregame/pregame.cpp
@@ -0,0 +1,21 @@
+#include "pregame.h"
+#include "../../gameplay/board/board.h"
+#include "  ../gameplay/ship/ship.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void displayHome() {
+    
+}
+
+void displaySetup() {
+
+}
+
+void startGame() {
+
+}
---
+++ backend/ui/pregame/pregame.h
@@ -0,0 +1,13 @@
+#ifndef PREGAME_H
+#define PREGAME_H
+
+#include "../../gameplay/board/board.h"
+
+#include <string>
+#include <vector>
+
+void displayHome();
+void displaySetup();
+void startGame();
+
+#endif // PREGAME_H
