e763cda9a5ac815fd21152435a47a4fbd31b663f
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-14 07:15:24 +0000

    Migrate ship management functions to ship.h and implement their definitions in ship.cpp

---
+++ backend/gameplay/mechanics/ship/ship.cpp
@@ -0,0 +1,38 @@
+#include "board.h"
+#include "ship.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Board addShipToBoard(Board &board, Ship &ship, int row, int col, string direction) {
+    if (checkAvailableSpace(board, ship, row, col, direction)) {
+        string shipSymbol = "[" + ship.getName().substr(0, 1) + "]"; // e.g., "[W]" for "Wok"
+        if (direction == "horizontal") {
+            for (int i = 0; i < ship.getLength(); i++) {
+                board.getBoard()[row][col + i] = shipSymbol;
+            }
+        } else if (direction == "vertical") {
+            for (int i = 0; i < ship.getLength(); i++) {
+                board.getBoard()[row + i][col] = shipSymbol;
+            }
+        }
+        board.addShip(ship);
+        return board;
+    }
+}
+
+Board attackShipOnBoard(Board &board, int row, int col) {
+    if (!checkOutOfBounds(board, row, col)) {
+        if (!checkEmptyCell(board.getBoard()[row][col])) {
+            board.getBoard()[row][col] = "[X]"; // Hit
+        } else {
+            board.getBoard()[row][col] = "[.]"; // Miss
+        }
+    }
+    return board;
+}
+
+bool checkShipSunk(Ship &ship) {
+    return ship.checkIsSunk();    
+}
