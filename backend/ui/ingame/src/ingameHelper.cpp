03d428e8ef6594a39097d706e2bfef8730fd3a12
Vi <vihuynh27.work@gmail.com>
2025-09-14 12:33:43 -0700

    Add ingameHelper header and source files for board and ship display functions

---
+++ backend/ui/ingame/src/ingameHelper.cpp
@@ -0,0 +1,26 @@
+#include "../include/ingameHelper.h"
+#include "../../../gameplay/board/board.h"
+#include "../../../gameplay/ship/ship.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void printBoard(Board& board) {
+
+}
+
+void printShipLists(Board& board) {
+
+}
+
+void printShipInfo(Ship& ship) {
+
+}
+
+void printCommentary() {
+
+}
+
