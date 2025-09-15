95c6e043dfea079e09798bb744bffd256ae23ef2
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-15 05:00:24 +0000

    Refactor printBoard function to enhance board display with row and column indices

--- backend/ui/ingame/src/ingameHelper.cpp
+++ backend/ui/ingame/src/ingameHelper.cpp
@@ -9,15 +9,23 @@
 using namespace std;
 
 void printBoard(Board& board) {
+    cout << "\n" << endl;
     cout << board.getPlayerName() << "'s Board: " << endl;
+    // i want to print it like a chessboard with numbers on top and side
+    cout << "  ";
+    for (int j = 0; j < board.getColumns(); j++) {
+        cout << "  " << j << " ";
+    }
+    cout << endl;
+
     for (int i = 0; i < board.getRows(); i++) {
+        cout << " "  << i << " ";
         for (int j = 0; j < board.getColumns(); j++) {
             cout << board.getBoard()[i][j] << " ";
         }
         cout << endl;
     }
     cout << endl;
-    printShipList(board);
 }
 
 void printShipList(Board& board) {
