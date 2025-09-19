3a1d46d08f188adbe9aba2325c8e5787a939ba52
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-19 03:18:13 +0000

    Add bot header and source files for randomized board creation

---
+++ backend/gameplay/mechanics/bot/include/bot.h
@@ -0,0 +1,8 @@
+#ifndef MECHANICS_BOT_H
+#define MECHANICS_BOT_H
+
+#include <string>
+#include "../../../board/include/board.h"
+#include "../../../ship/include/ship.h"
+
+Board createRandomizedBoard(int, int, std::string);
