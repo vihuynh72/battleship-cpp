a84c2d2ad41825b41e631b49da52867e4abb9e29
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-22 03:33:57 +0000

    Refactor Game class to include getter and setter methods for player boards, PvP status, and game rules

--- backend/gameplay/game/include/game.h
+++ backend/gameplay/game/include/game.h
@@ -9,7 +9,21 @@ class Game {
         Game();
         Game(Board& p1Board, Board& p2Board);
         Game(Board& p1Board, Board& p2Board, bool pvp, GameMode& gameRules);
-            
+
+        //getter setters for players' boards
+        Board& getPlayer1Board() { return player1Board; };
+        Board& getPlayer2Board() { return player2Board; };
+        void setPlayer1Board(Board& board) { player1Board = board; };
+        void setPlayer2Board(Board& board) { player2Board = board; };
+
+        // getter setter for if PvP
+        bool getIsPvP() { return isPvP; };
+        void setIsPvP(bool pvp) { isPvP = pvp; };
+
+        // getter setter for game rules
+        GameMode& getGameRules() { return rules; };
+        void setGameRules(GameMode& gameRules) { rules = gameRules; };
+
     private:
         Board& player1Board;
         Board& player2Board;
