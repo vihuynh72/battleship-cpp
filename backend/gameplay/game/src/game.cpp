2b76c34541d9cebe9be46bcbc06fce86eb36d2c5
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-09-22 03:29:48 +0000

    Add overloaded Game constructor to initialize with player boards for PVC games

--- backend/gameplay/game/src/game.cpp
+++ backend/gameplay/game/src/game.cpp
@@ -4,4 +4,8 @@
 Game::Game() : player1Board(nullptr), player2Board(nullptr), isPvP(false), rules(*(new GameMode())) {
     // Default constructor implementation
 }
+
+Game::Game(Board& p1Board, Board& p2Board) : player1Board(p1Board), player2Board(p2Board), isPvP(false), rules(*(new GameMode())) {
+    // Constructor with boards to start a PVC game
+}
   
