10127c5171ee918e60ef9adc7798a0f5fd0133c8
Vi <165195947+vihuynh72@users.noreply.github.com>
2025-10-03 02:55:41 +0000

    Add starting game messages to startPvcGame and startPvPGame functions for improved user experience

--- backend/ui/ingame/src/startgame.cpp
+++ backend/ui/ingame/src/startgame.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 void startPvcGame(Board& player1, Board& player2) {
+    cout << "================================" << endl;
+    cout << "         STARTING GAME          " << endl;
     cout << "Starting game between " << player1.getPlayerName() << " and " << player2.getPlayerName() << "!" << endl;
     cout << "Let the battle begin!" << endl;
 
@@ -17,6 +19,8 @@ void startPvcGame(Board& player1, Board& player2) {
 }
 
 void startPvPGame(Board& player1, Board& player2) {
+    cout << "================================" << endl;
+    cout << "         STARTING GAME          " << endl;
     cout << "Starting game between " << player1.getPlayerName() << " and " << player2.getPlayerName() << "!" << endl;
     cout << "Let the battle begin!" << endl;
 
